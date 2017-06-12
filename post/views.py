455985d0d4a2e18347d727378952bcd08ba943e1
Maksim Manylow <maksimmanylow@gmail.com>
2017-06-12 20:51:35 +0300

    index page added pagination

--- post/views.py
+++ post/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     return HttpResponse('Index function view')
@@ -9,6 +10,18 @@ def index(request):
 class IndexView(View):
     def get(self, request):
         posts = Post.objects.filter(is_published=True)
+        paginator = Paginator(posts, 2)
+        page = request.GET.get('page')
+
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
+
         return render(request, 'post/index.html', {'posts': posts})
 
 
