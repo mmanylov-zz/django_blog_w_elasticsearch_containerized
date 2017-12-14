2756e9e8da03f2966aa38a2e8df2aef14e632f34
Maksim Manylow <maksimmanylow@gmail.com>
2017-12-14 16:36:47 +0300

    fixed admin urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^9104da4ec86c28b6ee2bc84fb2870a659c170864/', admin.site.urls),
     url(r'^dashboard/', include('dashboard.urls', namespace='dashboard', app_name='dashboard')),
     url(r'', include('post.urls', namespace='post', app_name='post')),
 ]
