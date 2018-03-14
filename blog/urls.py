7d37ffc17995f3aa7667ed8025445b53232cf5c8
Maksim Manylow <maksimmanylow@gmail.com>
2018-03-14 23:51:39 +0300

    moved to Django v2

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,25 @@
-from django.conf.urls import url, include
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import include, path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^9104da4ec86c28b6ee2bc84fb2870a659c170864/', admin.site.urls),
-    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard', app_name='dashboard')),
-    url(r'', include('post.urls', namespace='post', app_name='post')),
-]
+    path('9104da4ec86c28b6ee2bc84fb2870a659c170864/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
+    path('post/', include('post.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/urls.py
+++ post/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url
-
+from django.urls import path, re_path
 from .views import IndexView, PostView#, AboutView, ContactsView
 
+app_name = 'post'
+
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index'),
     #url(r'^index/$', IndexView.as_view()),
     #url(r'^about/$', AboutView.as_view()),
     #url(r'^contacts/$', ContactsView.as_view()),
-    url(r'^post/(?P<slug>[-\w]+).html$', PostView.as_view(), name='post'),
+    re_path(r'^post/(?P<slug>[-\w]+).html$', PostView.as_view(), name='post'),
 ]
